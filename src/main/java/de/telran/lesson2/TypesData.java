6973e48a3ce3bfef81829074ac8ac8f6e65c3626
AlinaKobetss <alinakai88@gmail.com>
2023-09-26 11:33:26 +0200

    lesson 2 - 1

---
+++ src/main/java/de/telran/lesson2/TypesData.java
@@ -0,0 +1,55 @@
+package de.telran.lesson2;
+
+public class TypesData {
+
+
+    public static void main(String[] args) {
+
+        int myInt;
+        myInt = 25;
+        System.out.println(myInt);
+        int myIntOther = 10;
+
+        myInt = 13;
+
+        System.out.println(myInt);
+
+
+        //Целочисленные типы
+
+        byte myByte = 18;  //  -128...127
+        short myShort = 200; //  -32768....32767
+        int myInt1 = 40000; // -30000000...30000000
+        long myLong = 100000;  // очень большое число
+
+        //Дробные числа
+
+        float myFloat = 3.14F;
+        double myDouble = 1000.85;
+
+        char myChar = 'd'; // для хранения символов
+        String myStr = "мама мыла раму"; // для хранения строки
+
+        boolean myBool = true; // false
+
+        System.out.println("myByte = "+myByte);
+        System.out.println("myShort = "+myShort);
+        System.out.println("myInt1 = "+myInt1);
+        System.out.println("myLong = "+myLong);
+        System.out.println("myFloat = "+myFloat);
+        System.out.println("myDouble = "+myDouble);
+        System.out.println("myChar = "+myChar);
+        System.out.println("myStr = "+myStr);
+        System.out.println("myBool = "+myBool);
+
+
+
+
+
+
+
+
+
+
+    }
+}
