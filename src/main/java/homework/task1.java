963d55b60613ea1da1a58e6439c7ff6c1c1b742c
AlinaKobetss <alinakai88@gmail.com>
2023-10-31 21:32:11 +0100

    Homework task 1-2

---
+++ src/main/java/homework/task1.java
@@ -0,0 +1,36 @@
+package homework;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class task1 {
+        public static void main(String[] args) {
+            int[] randomArr = generateRandomArr(8, 1, 50);
+            replaceOddAndPrint(randomArr);
+        }
+        private static int[] generateRandomArr(int size, int from, int to) {
+            Random random = new Random();
+            int[] arr = new int[size];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = random.nextInt(to) + from;
+            }
+            System.out.println("Массив с " + size + "-ю случайными числами от " + from + " до " + to + " сгенерирован " + Arrays.toString(arr));
+            return arr;
+        }
+        private static void replaceOddAndPrint(int[] arr) {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] % 2 != 0) arr[i] = 0;
+            }
+            System.out.println("В массиве все нечетные числа заменены на ноль: " + Arrays.toString(arr));
+        }
+    }
+
+
+//Создайте массив из 8 случайных целых чисел из интервала [1;50]
+//Выведите массив на консоль в строку.
+//Замените каждый элемент с нечетным индексом на ноль.
+//Снова выведете массив на консоль в отдельной строке.
+
+
+
+
---
+++ src/main/java/homework/task2.java
@@ -0,0 +1,31 @@
+package homework;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class task2 {
+        public static void main(String[] args) {
+
+            Random rnd = new Random();
+            int[] arrInt = new int[5];
+            int example = 0;
+            boolean isSorted = true;
+            for (int i = 0; i < arrInt.length; i++) {
+                arrInt[i] = rnd.nextInt((99 - 10 + 1) + 10);
+                if (arrInt[i] < example) {
+                    isSorted = false;
+                }
+                example = arrInt[i];
+            }
+            System.out.println(Arrays.toString(arrInt));
+            System.out.println(isSorted ? "Массив строго возрастающий" : "Массив не строго возрастающий");
+        }
+    }
+
+
+//Создайте массив из 5 случайных целых чисел из интервала [10;99]
+//Выведите его на консоль в строку.
+//Определите и выведите на экран сообщение о том,
+// является ли массив строго возрастающей последовательностью.
+
+
