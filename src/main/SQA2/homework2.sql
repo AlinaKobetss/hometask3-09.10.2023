d505b5d14cf094764f801a74dc6c0e7dd112828a
AlinaKobetss <alinakai88@gmail.com>
2023-12-16 22:51:35 +0100

    Homework 2 sqa

---
+++ src/main/SQA2/homework2.sql
@@ -0,0 +1,32 @@
+1. Вывести данные о товарах (название, цена со скидкой в 0.5%).
+
+SELECT
+ProductName,
+Price,
+Price - Price * 0.005 AS Price_with_discount
+FROM Products
+
+2. Вывести самый дорогой товар в диапазоне до 100 EUR.
+
+SELECT *
+FROM Products
+WHERE
+Price < 100
+ORDER BY Price DESC
+LIMIT 1
+
+3. Вывести два самых дешевых товара из категории 4.
+
+SELECT *
+FROM Products
+WHERE
+CategoryID = 4
+ORDER BY Price ASC
+LIMIT 2
+
+4. Вывести один товар, который находится на пятом месте среди самых дорогих.
+
+SELECT *
+FROM Products
+ORDER BY Price DESC
+LIMIT 1 OFFSET 4
